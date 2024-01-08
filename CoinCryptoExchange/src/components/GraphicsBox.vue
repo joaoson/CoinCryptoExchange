8b0dcd7a6d12ba57147cdbbdb5be990572496655
Joao Freitas <joaovitor6586@gmail.com>
2024-01-08 14:52:10 -0300

    created graphics box

---
+++ CoinCryptoExchange/src/components/GraphicsBox.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="cardHeader">
+        <img src="{{url}}" alt="icon">
+        <div class="itemDetails">
+            <h3>{{ coinName }}</h3>
+            <h4>{{ coinAbrev }}</h4>
+        </div>
+    </div>
+    <div class="graphics">
+        <div class="numbers">
+            <p>{{ price }}</p>
+            <p>{{ percentile }}</p>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
