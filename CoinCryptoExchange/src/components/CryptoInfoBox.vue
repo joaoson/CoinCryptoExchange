771fdb188a82524193d490906be6bf6169acb899
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2024-01-08 15:35:57 -0300

    CryptoInfoBox

---
+++ CoinCryptoExchange/src/components/CryptoInfoBox.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+
+    <div class="data">
+        <div class="image-square">
+            <img src="" alt="imagem">
+        </div>
+        <div class="info">
+            <h1 class="name">{{ name }}</h1>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.data {
+    padding-left: 30px;
+}
+.image-square {
+    width: 45px;
+    height: 45px;
+    background-color: grey;
+    border-radius: 7px;
+}
+.name {
+    color: white;
+    font-size: 15px;
+    margin: auto;
+}
+</style>
--- CoinCryptoExchange/src/components/ListBox.vue
+++ CoinCryptoExchange/src/components/ListBox.vue
@@ -1,18 +1,26 @@
 <script setup>
+    import CryptoInfoBox from './CryptoInfoBox.vue';
 </script>
 
 <template>
 
     <div class="square">
         <h1 class="title">Real-time price</h1>
-        
-        <div class="data">
-            
-        </div>
+        <CryptoInfoBox name="Etherium" />
     </div>
 
 </template>
 
 <style scoped>
-
+.square {
+    background-color: #1b2028;
+    width: 400px;
+    height: 500px;
+    color: white;
+    border-radius: 10px;
+}
+.title{
+    padding: 30px;
+    font-size: 25px;
+}
 </style>
